0ee1d2bdeed8d3150068dd34e2f016a039af2f18
Arshad0786 <js07150751@gmail.com>
2020-02-27 17:03:40 +0800

    Reconstruct code to fix all the issues and added comment

--- TwoSum.py
+++ TwoSum.py
@@ -1,14 +1,23 @@
 class Solution:
     def twoSum(self, nums, target):
         for i in range(len(nums)-1):
-            if ((nums[i]+nums[i+1]) == target - nums[i]):
-                ls = [i,i+1]
-                return ls
-            
-            
-            
-        
-output = Solution()
-aList = [1,3,8,9]
+            tempList = nums.copy()
+            # Create a list that doesn't include current element (index = i)
+            tempList.pop(i)
+            for j in range(len(tempList)):
+                # print([tempList[j],target,nums[i]])
+                if (tempList[j] == target - nums[i]):
+                    ls = [i, nums.index(tempList[j])]
+                    return ls
+        else:
+            return None
 
-print(output.twoSum(aList,17))
+
+"""
+explain : if A + B = target, then A = target - B,
+so if we have a list [3,7,12,4],
+we scan through the elements one by one to check if the rest of elements in the list
+equals target - current element.
+Ex:set target to 15, we choose 3 -> we create a list of [7,12,4], then we scan if 7, 12, 4 = 15 - 3
+12 does, then we output the index of both 3 and 12.
+"""
